1de37f17c3f75cc7b74c32a58f56b5806d805847
knightliao <knightliao@gmail.com>
2016-02-26 20:43:20 +0800

    re

--- pfrock_plugin_static/__init__.py
+++ pfrock_plugin_static/__init__.py
@@ -1,4 +1,6 @@
 from pfrock.core.register import PfrockPluginRegister
 from pfrock_plugin_static.index import PfrockStaticPlugin
 
+__version__ = '0.2.1.dev4'
+
 PfrockPluginRegister.register(PfrockStaticPlugin)
